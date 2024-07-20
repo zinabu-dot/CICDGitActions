d2bfb1ff22fda3050ef52c6b77074ba84e5ed06f
zinabu <65596664+zinabu-dot@users.noreply.github.com>
2024-07-20 17:18:33 +0200

    Update looped_sum.py

--- looped_sum.py
+++ looped_sum.py
@@ -1,11 +1,10 @@
 def while_loop(a: int) -> int:
     b = 0
     while a < 10:
-        b+=a
-        a+=1
+        b += a
+        a += 1  # Increment 'a' to eventually break the loop
     return b
 
-if__name__ == '__main__':
+if __name__ == '__main__':
     forward_sum = while_loop(6)
     print(forward_sum)
-    
